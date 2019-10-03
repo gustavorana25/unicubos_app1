e46c3639607013de902b09959547046a026c98a3
Gustavo Raña <gustavo.rana@cubos.io>
2019-10-03 20:43:59 -0300

    picasso, toast, change colors and strings

--- app/src/main/java/com/example/unicubos_app1/MainActivity.kt
+++ app/src/main/java/com/example/unicubos_app1/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.unicubos_app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Picasso
+            .get()
+            .load("https://vignette.wikia.nocookie.net/fanintendo/images/6/69/544px-Main_%287%29_%2813%29.png/revision/latest")
+            .into(imageView)
     }
+
+    override fun onStart() {
+        super.onStart();
+        toastMessage("Hi people", 40000);
+
+    }
+
+    fun toastMessage(text:String, duration:Int) = Toast.makeText(this, text, duration).show()
 }
